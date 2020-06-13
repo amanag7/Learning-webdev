a5191fba41848345aca9f916d97b04f5ff6b01d6
Aman Agrawal <sprshag@gmail.com>
2020-06-13 10:39:42 +0530

    Added practice pages and made button a little functional

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Learning-webdev
 Just trying some things out 'cause bored.
+
+
+# A few practice pages:
+
+-[Instapolaroid](https://apu77z.mimo.run/index.html)
+
+-[Profile](https://c28kes.mimo.run/index.html)
+
+-[Portfolio](https://daeg5u.mimo.run/index.html)
+
+-[Homeflix-homepage](https://4kslie.mimo.run/index.html)
+
+-[CSS-Practice](https://iewmld.mimo.run/index.html)
+
+-[Dark-Mode](https://nqqv3i.mimo.run/index.html)
--- files/script.js
+++ files/script.js
@@ -1,14 +1,16 @@
-var budget = 2900;
-var savingsPriority = "low";
+function calcbudget() {
+	var budget = 2900;
+	var savingsPriority = "low";
 
-var rent = 500;
-var utilities = 800;
-var newLaptop = 1200;
+	var rent = 500;
+	var utilities = 800;
+	var newLaptop = 1200;
 
-var moneyLeft = budget - (rent + utilities);
-var buyLaptop = moneyLeft >= newLaptop;
-var save = savingsPriority!=="high";
+	var moneyLeft = budget - (rent + utilities);
+	var buyLaptop = moneyLeft >= newLaptop;
+	var save = savingsPriority!=="high";
 
-document.getElementById("canyou").innerHTML = "Can you buy laptop?: " + buyLaptop;
-document.getElementById("shouldyou").innerHTML = "Should you buy laptop?: " + save;
-document.getElementById("money").innerHTML = "Money left after buying said laptop: " + (moneyLeft-newLaptop);
+	document.getElementById("canyou").innerHTML = "Can you buy laptop?: " + buyLaptop;
+	document.getElementById("shouldyou").innerHTML = "Should you buy laptop?: " + save;
+	document.getElementById("money").innerHTML = "Money left after buying said laptop: " + (moneyLeft-newLaptop);
+}
