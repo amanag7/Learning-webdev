a2083eebafc271444e24d2287f89f2945e8ec2ab
Aman Agrawal <sprshag@gmail.com>
2020-06-13 11:01:44 +0530

    A few CSS changes

--- site-1/script.js
+++ site-1/script.js
@@ -7,10 +7,21 @@ function calcbudget() {
 	var newLaptop = 1200;
 
 	var moneyLeft = budget - (rent + utilities);
-	var buyLaptop = moneyLeft >= newLaptop;
-	var save = savingsPriority!=="high";
+
+	if(moneyLeft >= newLaptop) {
+		buyLaptop = "Sure why not";
+		document.getElementById("money").innerHTML = "Money left after buying said laptop: " + (moneyLeft-newLaptop);
+	} else {
+		buyLaptop = "Nah, you broke";
+	}
+
+	if(savingsPriority!=="high") {
+		save = "Yeah, go for it!";
+	} else {
+		save = "Probably not";
+	}
 
 	document.getElementById("canyou").innerHTML = "Can you buy laptop?: " + buyLaptop;
 	document.getElementById("shouldyou").innerHTML = "Should you buy laptop?: " + save;
-	document.getElementById("money").innerHTML = "Money left after buying said laptop: " + (moneyLeft-newLaptop);
+	
 }
